ff900ecac4481225a763e678919744265295cbb7
Arvind Kumar <arvind.kumar2@avisbudget.com>
2025-08-04 23:43:54 +0530

    Installed neccessary project and wrote initial code to coonect and initialize the project

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+
+// Express setup
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const server = http.createServer(app);
+
+// Database connection and server start
+mongoose
+  .connect(process.env.db_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+    server.listen(process.env.PORT, () => {
+      console.log("hey server is listening on Port no - ", process.env.PORT);
+    });
+  });
